fb13844072a8ef432f07a9b15d6327f760ffcead
MMKLOUD-source <m.mohamed.i@outlook.fr>
2025-02-24 11:20:43 +0100

    Update outputs.tf

--- labs/01-setup-entra-tenant/terraform/outputs.tf
+++ labs/01-setup-entra-tenant/terraform/outputs.tf
@@ -1,1 +1,17 @@
+# outputs.tf - Sorties de Terraform
+
+output "tenant_id" {
+  description = "ID du tenant Entra ID créé"
+  value       = azurerm_ad_tenant.entra_tenant.id
+}
+
+output "admin_username" {
+  description = "Nom d'utilisateur de l'administrateur créé"
+  value       = var.admin_username
+}
+
+output "tenant_domain" {
+  description = "Domaine par défaut du tenant Entra ID"
+  value       = "${var.tenant_name}.onmicrosoft.com"
+}
 
