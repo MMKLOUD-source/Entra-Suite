95fa61352d16124186549132ac66d3e32ccf6912
MMKLOUD-source <m.mohamed.i@outlook.fr>
2025-02-24 11:20:14 +0100

    Update variables.tf

--- labs/01-setup-entra-tenant/terraform/variables.tf
+++ labs/01-setup-entra-tenant/terraform/variables.tf
@@ -1,1 +1,25 @@
+# variables.tf - Définition des variables pour Terraform
 
+variable "tenant_name" {
+  description = "Nom du tenant Entra ID à créer"
+  type        = string
+  default     = "MyEntraTenant"
+}
+
+variable "location" {
+  description = "Emplacement Azure pour les ressources associées"
+  type        = string
+  default     = "francecentral"
+}
+
+variable "admin_username" {
+  description = "Nom d'utilisateur de l'administrateur Entra ID"
+  type        = string
+  default     = "admin@myentratenant.onmicrosoft.com"
+}
+
+variable "admin_password" {
+  description = "Mot de passe de l'administrateur Entra ID"
+  type        = string
+  sensitive   = true
+}
