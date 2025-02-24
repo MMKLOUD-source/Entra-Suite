0bb721966632db1adf0920c127d24aef473f4b28
MMKLOUD-source <m.mohamed.i@outlook.fr>
2025-02-24 11:17:51 +0100

    Update main.tf

--- labs/01-setup-entra-tenant/terraform/main.tf
+++ labs/01-setup-entra-tenant/terraform/main.tf
@@ -1,1 +1,15 @@
+# Terraform - Création d'un tenant Entra ID
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_ad_tenant" "entra_tenant" {
+  display_name = "MyEntraTenant"
+  country      = "FR"
+}
+
+output "tenant_id" {
+  value = azurerm_ad_tenant.entra_tenant.id
+}
 
